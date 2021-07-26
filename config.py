51057a9ad1ad353db45564907fe22cbf260b9875
Mauricio <mauxms1996@gmail.com>
2021-07-26 12:00:41 -0400

    Add config.py file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Tupla ("NombreDeLaCuenta",RoninWallet)
+# sustituye ronin:0000... por 0x0000... 
+ACCOUNTS = [
+    ("ME","0xf392e0298bc19b969f131682ab46fde0f80420b8"),      
+    ("MOM","0x11b707cee1a3888d17be7f5d175261c94f90f038"),
+]
+
+# SLP REQUERIDOS
+TOTAL_DESIRED = 1500
--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 import datetime
 import requests 
 from datetime import datetime,timedelta
+from config import ACCOUNTS,TOTAL_DESIRED
 import colorama
 
 API_URL = "http://api.lunaciarover.com/stats/" 
-
-
-# Tupla ("NombreDeLaCuenta",RoninWallet)
-# sustituye ronin:0000... por 0x0000... 
-RONINS = [
-    ("ME","0xf392e0298bc19b969f131682ab46fde0f80420b8"),      
-    ("MOM","0x11b707cee1a3888d17be7f5d175261c94f90f038"),   
-  	
-]
-
-#slp requeridos!
-TOTAL_DESIRED = 1500
 
 def get_ronin_info(r):
     return requests.api.get(API_URL + r ).json()
@@ -27,7 +16,7 @@ def colorize_slp(slp):
     return colorama.Fore.RED + str(slp) + colorama.Fore.RESET
 
 def print_ronin_data():
-    for name,ronin in RONINS:
+    for name,ronin in ACCOUNTS:
         ronin_data = get_ronin_info(ronin)
         print(name,":")
         print("\t",colorize_slp(ronin_data["in_game_slp"]),"slp")
@@ -40,7 +29,6 @@ def print_ronin_data():
 
 def main():
     print("SLP REQUERIDO POR DEFECTO :",TOTAL_DESIRED)
-    
     print("---------------------------------")
     print_ronin_data()
     pass
